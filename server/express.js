1c9b55278fa929d99dc35b9bd06c7e075757822f
montekaka <josh.chen.jia@gmail.com>
2019-07-09 20:47:56 -0700

    adds start helper method connection to server

--- server/express.js
+++ server/express.js
@@ -1,13 +1,28 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
+const connect = require('./utils/db');
+const config = require('./../config/config');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 require('./routes.js')(app);
 
-module.exports = app;
+const start = async () => {
+  try {
+    await connect();
+    app.listen(config.port, () => {
+      console.log(`REST API on http://localhost:${config.port}/api`)
+    })
+  } catch(e) {
+    console.error(e);
+  }
+}
+
+module.exports = start;
