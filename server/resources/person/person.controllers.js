ab3f2b54099ea0052e68df50ff679064cd66be01
montekaka <josh.chen.jia@gmail.com>
2019-07-14 21:12:09 -0700

    adds getSignedCerts to list a certs signed info

--- server/resources/person/person.controllers.js
+++ server/resources/person/person.controllers.js
@@ -1,21 +1,20 @@
 const neode = require('../index');
 const uuidv4 = require('uuid/v4');
 
-
- const get = (req, res) => {
-   const query = 'match (p:Person) return p';
-   neode.cypher(query, {})
-   .then((collection) => {    
-     const resdata = collection.records.map((item) => {
-       return item['_fields'][0]['properties']
-     });
-     res.status(202).json({data: resdata});
-   })
-   .catch((err) => {
-     console.log('err')
-     res.status(404).json(err);
-   })   
- }
+const get = (req, res) => {
+  const query = 'match (p:Person) return p';
+  neode.cypher(query, {})
+  .then((collection) => {    
+    const resdata = collection.records.map((item) => {
+      return item['_fields'][0]['properties']
+    });
+    res.status(202).json({data: resdata});
+  })
+  .catch((err) => {
+    console.log('err')
+    res.status(404).json(err);
+  })   
+}
 
 const getWithCerts = (req, res) => {
   const query = "match (p:Person)-[r:HAS_CERTIFICATION]->(c:Certification) return {person: p, cert: collect({name:c.name, id:c.id, expirationDate:r})}"
@@ -23,7 +22,11 @@ const getWithCerts = (req, res) => {
   .then((collection) => {
     const resdata = collection.records.map((item) => {
       const fields = item['_fields'][0];
-      return {person: fields['person']['properties'], certs: fields['cert']}
+      const certs = fields['cert'].map((cert) => {
+        return {name: cert['name'], id: cert['id'], expirated_at: cert['expirationDate']['properties']['expirated_at']}
+      });
+      
+      return {person: fields['person']['properties'], certifications: certs}
     });
     res.status(202).json({data: resdata});
   })
@@ -67,7 +70,7 @@ const create = (req, res) => {
 const addCertification = (req, res) => {
   const personId = req.params.id;
   const certificationId = req.body.certificationId;
-  const expriationDate = new Date(req.body.expriationDate);
+  const expriationDate = (new Date(req.body.expriationDate)).getTime();
   
   Promise.all([
     neode.first('Person', 'id', personId),
@@ -88,7 +91,7 @@ const signCertification = (req, res) => {
   const certId = req.params.id;
   const signPersonID = req.body.person_id;
   const personId = req.params.person_id;
-  const signAt = new Date(req.body.sign_at);
+  const signAt = (new Date(req.body.sign_at)).getTime();
 
   Promise.all([
     neode.first('Person', 'id', signPersonID),
@@ -106,10 +109,29 @@ const signCertification = (req, res) => {
   })
 }
 
+const getSignedCerts = (req, res) => {
+  const personId = req.params.person_id;
+  const certId = req.params.id;
+  const query = 'MATCH (p:Person)-[r:SIGNS_CERTIFICATION {person_id: {person_id}}]->(c:Certification {id:{certification_id}}) return {signed_person: p, signed_at: r.signed_at}'
+  neode.cypher(query, {person_id: personId, certification_id: certId})
+  .then((collection) => {
+    const resdata = collection.records.map((item) => {
+      const fields = item['_fields'][0];  
+      return {person: fields['signed_person']['properties'], signed_at: fields['signed_at']}
+    });
+    res.status(202).json({data: resdata});
+  })
+  .catch((err) => {
+    console.log('err')
+    res.status(404).json(err);
+  })  
+}
+
 module.exports = {
   get: get,
   getWithCerts: getWithCerts,  
   create: create,
   addCertification: addCertification,
   signCertification: signCertification,  
+  getSignedCerts: getSignedCerts,
 }
