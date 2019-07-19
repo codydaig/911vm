18ff1a7dd0f2488c63d6ed8ab383c03e8db75463
montekaka <josh.chen.jia@gmail.com>
2019-07-18 22:40:10 -0700

    deleted test model

--- server/resources/test/test.router.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express').Router;
-const Controllers = require('./test.controllers');
-
-const router = Router();
-
-router.route('/').get(Controllers.get)
-router.route('/').post(Controllers.create)
-
-module.exports = router;
