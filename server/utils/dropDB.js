b2b58fb488e520921531e802d720ecb220a1c111
montekaka <josh.chen.jia@gmail.com>
2019-07-21 11:48:22 -0700

    add scrpt to drop db

---
+++ server/utils/dropDB.js
@@ -0,0 +1,11 @@
+const neode = require('./../resources/index');
+
+neode.cypher('MATCH (n) DETACH DELETE n',{})
+.then(() => {
+  console.log('Deleted all models from Database');
+  neode.close();
+})
+.catch((err) => {
+  console.log(err.message);
+  neode.close();
+})
