829e3a92e5cc9b3ddd8225b750c2d506f06cfd39
montekaka <josh.chen.jia@gmail.com>
2019-08-03 01:01:34 -0700

    removes function on each request to model

--- server/controllers/certification.controllers.js
+++ server/controllers/certification.controllers.js
@@ -1,18 +1,15 @@
-const neode = require('../schema/index');
+const models = require('./../models');
+const Certifications = models.Certifications;
 
 const get = (req, res) => {
-  const query = 'match (c:Certification) return c';
-  neode.cypher(query, {})
-  .then((collection) => {    
-    const resdata = collection.records.map((item) => {
-      return item['_fields'][0]['properties']
-    });
-    res.status(202).json({data: resdata});
+  Certifications.getAll()
+  .then((data) => {
+    res.status(200).json({data: data});
   })
   .catch((err) => {
     console.log('err')
     res.status(404).json({error_message: err.message});
-  })    
+  })     
 }
 
 // create a new type of certification
@@ -20,37 +17,18 @@ const create = (req, res) => {
   let data = {
     name: req.body.name
   }
-  neode.create('Certification', data)
+  
+  Certifications.addOne(data)
   .then((certification) => {
-    res.status(200).json({data: {
-      name: certification.get('name'),
-      createdAt: certification.get('created_at'),
-    }});
+    res.status(200).json({data: certification});
   })
   .catch((err) => {
-    console.error(err);
+    console.log('err')
     res.status(404).json({error_message: err.message});
-  })
+  })  
 }
-
-//sign offs a volunteer's certification
-// const signs = (req, res) => {
-//   const certificationId = req.params.id;
-//   const personId = req.body.person_id;
-//   const signOffPersonId = req.body.sign_off_person_id;
-//   const signOffDate = (new Date()).getTime();
-//   const query = "match (p1:Person {id:{signOffPersonId}}), ((p:Person {id:{personId}})-[:HAS_CERTIFICATION]->(c:Certification {id:{certificationId}})) create (p1)-[:SIGNS_CERTIFICATION {person_id:{personId}, signed_at:{signOffDate}}]->(c)"
-//   neode.cypher(query, {certificationId: certificationId, personId: personId, signOffPersonId: signOffPersonId, signOffDate: signOffDate})
-//   .then(() => {
-//     res.status(202).json({message: "Relationship created"});
-//   })
-//   .catch((err) => {
-//     res.status(404).json({error_message: err.message});
-//   })
-// }
 
 module.exports = {
   create: create,
-  get: get,
-  // signs: signs
+  get: get
 }
