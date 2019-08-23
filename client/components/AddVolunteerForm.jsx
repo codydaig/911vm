092106c52070bc2470df213b3a299486d6fef96c
David Ramirez <davidraviles@gmail.com>
2019-08-22 22:58:33 -0700

    created one handleChange method for all elements.

---
+++ client/components/AddVolunteerForm.jsx
@@ -0,0 +1,165 @@
+import React from "react";
+import Input from "../elements/Input.jsx/index.js";
+import Select from "../elements/Select.jsx/index.js";
+import Checkbox from "../elements/Checkbox.jsx/index.js";
+import DateBox from "./DateBox.jsx/index.js.js";
+import SubmitButton from "../elements/SubmitButton.jsx/index.js";
+import axios from "axios";
+
+class AddVolunteerForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.reset = this.reset.bind(this);
+
+    this.state = {
+      newVolunteer: {
+        first_name: "",
+        last_name: "",
+        email_address: "",
+        phone_number: "",
+        class: "Probationary",
+        start_date: new Date(),
+        end_date: 0,
+        is_admin: false,
+        is_volunteer: false
+      },
+      classes: ["Probationary", "Trainee", "Apprentice", "General"],
+      initialVolunteer: {}
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ initialVolunteer: this.state.newVolunteer });
+  }
+
+  reset(e) {
+    e.preventDefault();
+    this.props.handleClose();
+    this.setState({ newVolunteer: this.state.initialVolunteer });
+  }
+
+  handleChange(e) {
+    //if date is being changed
+    if (e instanceof Date) {
+      //set date in milliseconds in volunteer object
+      this.setState(prevState => ({
+        newVolunteer: {
+          ...prevState.newVolunteer,
+          start_date: e.getTime()
+        }
+      }));
+      //if other info is being added
+    } else {
+      let value = e.target.value;
+      let property = e.target.name;
+
+      //if class is being selected
+      if (property === "is_admin" || property === "is_volunteer") {
+        this.setState(prevState => ({
+          newVolunteer: {
+            ...prevState.newVolunteer,
+            [property]: !this.state.newVolunteer[property]
+          }
+        }));
+        //if phone number is being added
+      } else if (property === "phone_number") {
+        let currentPhone = this.state.newVolunteer.phone_number;
+
+        value = value.replace(/\D/g, "");
+
+        if (currentPhone.length < 10) {
+          this.setState(prevState => ({
+            newVolunteer: { ...prevState.newVolunteer, [property]: value }
+          }));
+        }
+        //if other info is being added
+      } else {
+        this.setState(prevState => ({
+          newVolunteer: { ...prevState.newVolunteer, [property]: value }
+        }));
+      }
+    }
+  }
+
+  handleFormSubmit(e) {
+    e.preventDefault();
+    let userData = this.state.newVolunteer;
+
+    axios
+      .post(`/api/person`, userData)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    const showHideClassName = this.props.show
+      ? "form-bg visible"
+      : "form-bg not-visible";
+    return (
+      <div className={showHideClassName}>
+        <form className={"volunteerForm"} onSubmit={this.handleFormSubmit}>
+          <Input
+            name={"first_name"}
+            label={"First name"}
+            handle={this.handleChange}
+            value={this.state.newVolunteer.first_name}
+          />
+          <Input
+            name={"last_name"}
+            label={"Last name"}
+            handle={this.handleChange}
+            value={this.state.newVolunteer.last_name}
+          />
+          <Input
+            name={"email_address"}
+            label={"Email"}
+            handle={this.handleChange}
+            value={this.state.newVolunteer.email_address}
+          />
+          <Input
+            name={"phone_number"}
+            label={"Phone number"}
+            handle={this.handleChange}
+            value={this.state.newVolunteer.phone_number}
+          />
+          <Select
+            name={"class"}
+            label={"Select class"}
+            options={this.state.classes}
+            handle={this.handleChange}
+            selected={this.state.newVolunteer.class}
+          />
+          <Checkbox
+            name={"is_admin"}
+            title={"Administrator"}
+            option={"Administrator"}
+            handle={this.handleChange}
+            checked={this.state.newVolunteer.is_admin}
+          />
+          <Checkbox
+            name={"is_volunteer"}
+            title={"Volunteer"}
+            option={"Volunteer"}
+            handle={this.handleChange}
+            checked={this.state.newVolunteer.is_volunteer}
+          />
+          <DateBox
+            name={"start_date"}
+            label={"Start date"}
+            handle={this.handleChange}
+            selected={new Date(this.state.newVolunteer.start_date)}
+          />
+          <SubmitButton onSubmit={this.handleFormSubmit} />
+          <button onClick={this.reset}>Cancel</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddVolunteerForm;
