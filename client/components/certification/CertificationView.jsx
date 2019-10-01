08b31b3b686a2bb205e2de30adb65b3d19edb460
David Ramirez <davidraviles@gmail.com>
2019-10-01 01:24:46 -0700

    Displays all certification components.

--- client/components/certification/CertificationView.jsx
+++ client/components/certification/CertificationView.jsx
@@ -1,82 +1,109 @@
 import React from "react";
 import Certification from "./Certification.jsx";
 import axios from "axios";
+import EditingCertification from "./EditingCertification.jsx";
+import AddCertification from "./AddCertification.jsx";
 
 class CertificationView extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    // this.getVolunteers = this.getVolunteers.bind(this);
-    // this.getCertifications = this.getCertifications.bind(this);
-    // this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
 
     this.state = {
-      personInfo: {}, //volunteer info
-      personNames: [], //volunteer names for select elements
-      certifications: {}, //certification info
-      currentPerson: "", //currently selected person
-      currentId: "",
-      signOff: "",
       loaded: false,
-      editing: false
+      editing: false,
+      adding: false
     };
   }
 
   componentDidMount() {
-    //   console.log(this.props.certification)
+    let certTypes = {};
+
+    axios
+      .get("/api/certification")
+      .then(response => {
+        response.data.data.forEach(cert => {
+          certTypes[cert.name] = cert.id;
+        });
+        this.setState({
+          certificationTypes: certTypes,
+          loaded: true
+        });
+      })
+      .catch(error => {
+        console.log("error", error);
+      });
   }
 
-  handleClick() {
-    const editingValue = !this.state.editing;
-    this.setState({ editing: editingValue });
+  handleClick(e) {
+    const editingValue = this.state.editing;
+    const adding = this.state.adding;
+
+    if (e.target.name === "add-btn") {
+      if (!adding) {
+        this.setState({ adding: !adding });
+      }
+    } else if (
+      e.target.name === "cancel-btn" ||
+      e.target.name === "save-btn"
+    ) {
+      this.setState({ adding: !adding });
+    } else if (e.target.name === "edit-btn") {
+      this.setState({ editing: !editingValue });
+    }
   }
 
   render() {
-    return (
-      <div className="certifications-container">
-        <div className="certification-header">
-          <h2>Certifications</h2>
-        </div>
-        <div className="cell">
-          <h3>Name</h3>
-        </div>
-        <div className="cell">
-          <h3>Expiration Date</h3>
-        </div>
-        <div className="cell">
-          <h3>Sign Off</h3>
-        </div>
-        <div className="cell">
-          <h3>Edit</h3>
-        </div>
-        {this.props.certification.length !== 0 &&
-          this.props.certification.map((data, i) => {
-            return <Certification data={data} key={i} />;
-          })}
-        {/* {this.state.editing ? (
-          <div>
-            <button className="edit-certification" onClick={this.handleClick}>
-              Editing
-            </button>
-            <button className="save-btn" onClick={this.handleSaveClick}>
-              Save
+    if (this.state.loaded) {
+      return (
+        <div className="certifications-container">
+          <div className="certification-header">
+            <h2>Certifications</h2>
+            <button name="add-btn" onClick={this.handleClick}>
+              Add
             </button>
           </div>
-        ) : (
-          <div>
-            <button className="edit-certification" onClick={this.handleClick}>
-              Edit
-            </button>
-            <button
-              className="create-certification"
-              onClick={this.createCertification}
-            >
-              Add Certification
-            </button>
+          <div className="cell">
+            <h3>Name</h3>
           </div>
-        )} */}
-      </div>
-    );
+          <div className="cell">
+            <h3>Expiration Date</h3>
+          </div>
+          <div className="cell">
+            <h3>Sign Off</h3>
+          </div>
+          <div className="cell">
+            <h3>Sign Date</h3>
+          </div>
+          <div className="cell">
+            <h3>Actions</h3>
+          </div>
+          {this.props.certifications.length !== 0 &&
+            this.props.certifications.map((data, i) => {
+              return (
+                <Certification
+                  data={data}
+                  key={i}
+                  certificationTypes={this.state.certificationTypes}
+                />
+              );
+            })}
+          {this.state.adding ? (
+            <AddCertification
+              certificationTypes={this.state.certificationTypes}
+              signOffName={this.props.signOffName}
+              signOffId={this.props.signOffId}
+              personId={this.props.personId}
+              handleClick={this.handleClick}
+              updatePerson={this.props.updatePerson}
+            />
+          ) : null}
+        </div>
+      );
+    } else {
+      return null;
+    }
   }
 }
 
