1008b42cdbaee9f2610c966d235d431a854f9706
David Ramirez <davidraviles@gmail.com>
2019-11-25 21:44:11 -0800

    Added edit feature to personal info.

--- client/components/personal-info/PersonalInfoView.jsx
+++ client/components/personal-info/PersonalInfoView.jsx
@@ -7,72 +7,176 @@ import EditingPersonalInfo from "./EditingPersonalInfo.jsx";
 class PersonalInfoView extends React.Component {
   constructor(props) {
     super(props);
-    
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleStartDate = this.handleStartDate.bind(this);
+    this.updatePersonInfo = this.updatePersonInfo.bind(this);
+
     this.state = {
       editing: false,
-      email_address: "",
-      phone_number: "",
-      is_admin: "",
-      is_volunteer: "",
-      certifications: [],
-      volunteerNames: [],
-      volunteerInfo: {},
-      currentVolunteer: {}
+      initialData: {},
+      newData: {}
     };
-    // this.handleClick = this.handleClick.bind(this);
-    this.handlePersonalInfoChange = this.handlePersonalInfoChange.bind(this);
-    this.handleSaveClick = this.handleSaveClick.bind(this);
-    this.createCertification = this.createCertification.bind(this);
   }
 
-  handleSaveClick(e, saveAlteredCertifications) {
-    e.preventDefault();
-    // save to DB
-    const { email_address, phone_number, is_admin, is_volunteer } = this.state;
-    const isEditing = !this.state.editing;
-    const updatedInfo = {
-      email_address,
-      phone_number,
-      is_admin,
-      is_volunteer
-    };
-
-    this.setState({ editing: isEditing });
-    alert("saved to db" + JSON.stringify(updatedInfo));
-    // axios.put(`/api/person/${id}`, updatedInfo)
-    //   .then(response => {
-    //     console.log(response)
-    //   })
-    //   .catch(error => {
-    //     console.log("error", error)
-    //   })
-  }
-
-  handlePersonalInfoChange(event) {
-    event.preventDefault();
-    const id = event.target.id;
-    let value; 
-    if (id === "admin" || id === "volunteer") {
-      value = (event.target.value == "true")
-    } else {
-      value = event.target.value
+  componentDidMount() {
+    let data = this.props.personInfo;
+    if (!data.phone_number) {
+      data.phone_number = "";
     }
-    this.setState({[id]: value});
+    this.setState({ initialData: data });
+    this.setState({ newData: data });
+  }
+
+  componentDidUpdate(prevProps) {
+    if (
+      JSON.stringify(prevProps.personInfo) !==
+      JSON.stringify(this.props.personInfo)
+    ) {
+      this.setState({ initialData: this.props.personInfo });
+      this.setState({ newData: this.props.personInfo });
+    }
+  }
+
+  handleStartDate(e) {
+    this.setState(prevState => ({
+      newData: {
+        ...prevState.newData,
+        start_date: new Date(e).toISOString().split("T")[0]
+      }
+    }));
+  }
+
+  handleClick(e) {
+    const editing = this.state.editing;
+    const name = e.target.name;
+
+    if (name === "edit-btn") {
+      if (!editing) {
+        this.setState({ editing: !editing });
+      }
+    } else if (name === "save-btn") {
+      if (
+        this.state.newData.phone_number === null ||
+        this.state.newData.phone_number.length !== 9
+      ) {
+        alert("Invalid phone number.");
+      } else {
+        this.updatePersonInfo();
+        this.setState({ editing: !editing });
+      }
+    } else if (name === "cancel-btn") {
+      this.setState({ editing: !editing });
+    }
+  }
+
+  handleChange(e) {
+    const type = e.target.name;
+    let value = e.target.value;
+
+    //if first name is being changed
+    if (type === "first_name") {
+      this.setState(prevState => ({
+        newData: {
+          ...prevState.newData,
+          [type]: value
+        }
+      }));
+      //if last name is being changed
+    } else if (type === "last_name") {
+      this.setState(prevState => ({
+        newData: {
+          ...prevState.newData,
+          [type]: value
+        }
+      }));
+      //if email is being changed
+    } else if (type === "email_address") {
+      this.setState(prevState => ({
+        newData: {
+          ...prevState.newData,
+          [type]: value
+        }
+      }));
+      //if phone is being changed
+    } else if (type === "phone_number") {
+      value = value.replace(/\D/g, "");
+
+      this.setState(prevState => ({
+        newData: {
+          ...prevState.newData,
+          [type]: value
+        }
+      }));
+    }
+  }
+
+  updatePersonInfo() {
+    const updateData = {
+      id: this.props.personId,
+      certification_id: this.state.newData.id,
+      expriation_date: this.state.newData.expriation_date,
+      signature_date: this.state.newData.signature_date,
+      signature_person_id: this.state.newData.signature_person_id
+    };
+
+    axios
+      .put(`/api/person/${this.props.personInfo.id}`, this.state.newData)
+      .then(() => {
+        this.setState({ initialData: this.state.newData });
+        this.props.updatePerson();
+        alert("Successfully updated volunteer info :)");
+      })
+      .catch(error => {
+        alert("Error updating volunteer info :(");
+        console.log("error", error);
+      });
   }
 
   render() {
     const { personInfo } = this.props;
 
     return (
-      <div>
-        {editing ? (
+      <div className="personal-info">
+        <div className="personal-info-header">
+          <h1>
+            {personInfo.first_name} {personInfo.last_name}
+          </h1>
+        </div>
+        <div className="cell">
+          <h3>First Name</h3>
+        </div>
+        <div className="cell">
+          <h3>Last Name</h3>
+        </div>
+        <div className="cell">
+          <h3>Email</h3>
+        </div>
+        <div className="cell">
+          <h3>Phone Number</h3>
+        </div>
+        <div className="cell">
+          <h3>Class</h3>
+        </div>
+        <div className="cell">
+          <h3>Start Date</h3>
+        </div>
+        <div className="cell">
+          <h3>Actions</h3>
+        </div>
+        {this.state.editing ? (
           <EditingPersonalInfo
-            personInfo={personInfo}
+            personInfo={this.state.newData}
             onChange={this.handlePersonalInfoChange}
+            handleClick={this.handleClick}
+            handleChange={this.handleChange}
+            handleStartDate={this.handleStartDate}
           />
         ) : (
           <DefaultPersonalInfo
             personInfo={personInfo}
+            handleClick={this.handleClick}
           />
         )}
       </div>
